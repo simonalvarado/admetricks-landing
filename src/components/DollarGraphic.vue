3e484ef1b200777f273adcd9cec668ef71364c12
Simon Alvarado <s.alvarado@viseven.com>
2024-06-19 16:34:54 -0300

    update: added the filter option to the dollar graphic component, added some reactive data to the component, and added a new method to the component to filter the data

--- src/components/DollarGraphic.vue
+++ src/components/DollarGraphic.vue
@@ -1,14 +1,62 @@
 <template>
   <div class="dollar-graphic">
-    <div class="pb-5">
-      <div class="dollar-graphic__content col-12 mb-4">
-        <h2 class="text-center">Variación diaria del dólar</h2>
-        <p class="text-center">(en pesos chilenos, en el último mes)</p>
+    <div class="dollar-graphic__wrapper pb-5">
+      <div class="dollar-graphic__title col-12 mb-4">
+        <h2 class="text-center">
+          Gráficos de Admetricks: variación del USD en CLP
+        </h2>
+        <p class="text-center">
+          Admetricks Last Month Currency Exchanges Rate Chart
+        </p>
       </div>
-      <div class="dollar-graphic__image pt-4 col-12 text-center">
-        <svg class="dollar-graphic__image-desktop" ref="chartDesktop"></svg>
-        <svg class="dollar-graphic__image-mobile" ref="chartMobile"></svg>
-      </div>
+      <main class="dollar-graphic__content col-12">
+        <div class="dollar-graphic__content-wrapper">
+          <div class="dollar-graphic__text">
+            <div class="dollar-graphic__text__wrapper">
+              <div class="dollar-graphic__description">
+                <h1>Gráfico de variación del USD en CLP</h1>
+                <div class="dollar-graphic__variation">
+                  <h1
+                    :class="{
+                      'positive-variation': lastVariation > 0,
+                      'negative-variation': lastVariation < 0,
+                    }"
+                  >
+                    {{ lastVariation }}%
+                  </h1>
+                  <p>({{ timeRange }})</p>
+                </div>
+              </div>
+              <p>Dólar estadounidense a Peso chileno</p>
+            </div>
+            <div class="dollar-graphic__exchange-rate">
+              <p>
+                <b>1 USD = {{ lastValue }} CLP</b>
+              </p>
+              <p>{{ lastDate }}</p>
+            </div>
+          </div>
+          <div class="dollar-graphic__actions">
+            <button
+              class="dollar-graphic__button"
+              :class="{ 'dollar-graphic__button--active': timeRange === '1W' }"
+              @click="handleButtonClick('1W')"
+            >
+              1W
+            </button>
+            <button
+              class="dollar-graphic__button"
+              :class="{ 'dollar-graphic__button--active': timeRange === '1M' }"
+              @click="handleButtonClick('1M')"
+            >
+              1M
+            </button>
+          </div>
+          <div class="dollar-graphic__image pt-4 col-12 text-center">
+            <svg ref="chart" class="dollar-graphic__chart"></svg>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
@@ -22,26 +70,53 @@ export default {
   data() {
     return {
       dollarData: null,
+      filteredData: null,
+      width: 0,
+      timeRange: "1M",
     };
   },
   async mounted() {
+    window.addEventListener("resize", this.updateWidth);
+
     try {
       this.dollarData = await getDollarData();
-      console.log(this.dollarData);
-      this.drawChart(this.$refs.chartDesktop, 900, 500);
-      this.drawChart(this.$refs.chartMobile, 360, 300);
+      this.filterData();
+      this.width = this.$refs.chart.parentNode.offsetWidth;
+      this.drawChart();
     } catch (error) {
       console.log(error);
     }
   },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.updateWidth);
+  },
   methods: {
-    drawChart(chartRef, chartWidth, chartHeight) {
-      if (!this.dollarData) {
+    updateWidth() {
+      this.width = this.$refs.chart.parentNode.offsetWidth;
+      this.drawChart();
+    },
+
+    filterData() {
+      if (this.timeRange === "1W") {
+        this.filteredData = this.dollarData.serie.slice(0, 6);
+      } else {
+        this.filteredData = this.dollarData.serie;
+      }
+    },
+
+    handleButtonClick(timeRange) {
+      this.timeRange = timeRange;
+      this.filterData();
+      this.drawChart();
+    },
+
+    drawChart() {
+      if (!this.filteredData) {
         return;
       }
 
       // Reverse the data to have the most recent data first.
-      const data = [...this.dollarData.serie].reverse();
+      const data = [...this.filteredData].reverse();
 
       // Obtain the dates and variations from the data.
       const { dates, variations } = data.reduce(
@@ -59,9 +134,10 @@ export default {
       );
 
       // Declare the chart dimensions and margins.
-      const width = chartWidth;
-      const height = chartHeight;
-      const margin = { top: 20, bottom: 50, left: 30, right: 20 };
+      const width = this.width;
+      const height = 400;
+      const margin = { top: 20, right: 20, bottom: 50, left: 20 };
+
       // Declare the x (horizontal position) scale.
       const x = d3
         .scaleTime()
@@ -82,18 +158,29 @@ export default {
 
       // Create the SVG container.
       const svg = d3
-        .select(chartRef)
+        .select(this.$refs.chart)
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
+      // Clear the SVG container.
+      svg.selectAll("*").remove();
+
       // Add the x-axis.
-      svg
+      const xAxis = svg
         .append("g")
         .attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(x))
+        .call(d3.axisBottom(x).ticks(width < 600 ? 4 : 8)) // Adjust the number of ticks based on width
         .attr("font-size", "0.8rem");
+
+      if (width < 600) {
+        // Rotate labels for smaller viewports
+        xAxis
+          .selectAll("text")
+          .attr("transform", "rotate(-45)")
+          .style("text-anchor", "end");
+      }
 
       // Add the y-axis, remove the domain line, add grid lines and a label.
       svg
@@ -126,38 +213,190 @@ export default {
       return svg.node();
     },
   },
+  computed: {
+    lastVariation() {
+      if (this.timeRange === "1M") {
+        if (!this.dollarData || !this.dollarData.serie.length) return null;
+        const lastItem = this.dollarData.serie[0];
+        const monthAgoItem =
+          this.dollarData.serie[this.dollarData.serie.length - 1];
+        const variation =
+          ((lastItem.valor - monthAgoItem.valor) / monthAgoItem.valor) * 100;
+        return variation.toFixed(2);
+      } else {
+        if (!this.dollarData || !this.dollarData.serie.length) return null;
+        const lastItem = this.dollarData.serie[0];
+        const weekAgoItem = this.dollarData.serie[5];
+        const variation =
+          ((lastItem.valor - weekAgoItem.valor) / weekAgoItem.valor) * 100;
+        return variation.toFixed(2);
+      }
+    },
+    lastValue() {
+      if (!this.dollarData || !this.dollarData.serie.length) return null;
+      return this.dollarData.serie[0].valor;
+    },
+    lastDate() {
+      if (!this.dollarData || !this.dollarData.serie.length) return null;
+      const date = new Date(this.dollarData.serie[0].fecha);
+      const options = {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      };
+      return new Intl.DateTimeFormat("en-US", options).format(date);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .dollar-graphic {
-  margin: 80px 0 0px 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.dollar-graphic__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  padding: 80px 24px 0px 24px;
+  width: 100%;
+}
+.dollar-graphic__description {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+}
+.dollar-graphic__content {
+  display: flex;
+  justify-content: center;
+  background-color: #0b2a49;
+  border: 1px solid #bdbdbd;
+  border-radius: 5px;
+  padding: 48px;
+  width: 100%;
+}
+.dollar-graphic__content-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.dollar-graphic__text {
+  display: flex;
+  justify-content: space-between;
+}
+.dollar-graphic__text__wrapper p {
+  color: #a9b9c7;
+  font-size: 14px;
+  margin: 0 0 8px 0;
+}
+.dollar-graphic__description h1 {
+  font-size: 24px;
+  font-weight: 600;
+}
+.dollar-graphic__variation {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+.dollar-graphic__variation h1 {
+  font-size: 24px;
+  font-weight: 400;
+}
+.dollar-graphic__variation p {
+  margin-bottom: 8px;
+}
+.positive-variation {
+  color: #4fbd7d;
+}
+.positive-variation::before {
+  content: "+";
+}
+.negative-variation {
+  color: #ef5353;
+}
+.dollar-graphic__exchange-rate {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+.dollar-graphic__exchange-rate p {
+  color: #a9b9c7;
+  font-size: 14px;
+}
+.dollar-graphic__actions {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  border-top: 1px solid #bdbdbd24;
+  border-bottom: 1px solid #bdbdbd24;
+  padding: 20px 0;
+}
+.dollar-graphic__button {
+  background-color: #0b2a49;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 10px 20px;
+}
+.dollar-graphic__button--active {
+  background-color: #3fc3ef;
+  color: #0b2a49;
 }
 .dollar-graphic__image {
-  background-color: #0a2540;
   display: flex;
   justify-content: center;
 }
-.dollar-graphic__image svg {
-  display: none;
-}
-
-@media (min-width: 769px) {
-  .dollar-graphic__image svg.dollar-graphic__image-desktop {
-    display: block;
+@media (max-width: 1068px) {
+  .dollar-graphic__text {
+    flex-direction: column;
+  }
+  .dollar-graphic__description {
+    justify-content: flex-start;
+  }
+  .dollar-graphic__variation p {
+    margin-bottom: 0px;
   }
 }
 @media (max-width: 768px) {
-  .dollar-graphic {
-    margin: 40px 0 0px 0;
+  .dollar-graphic__title {
+    max-width: 345px;
   }
-  .dollar-graphic__image svg.dollar-graphic__image-mobile {
-    display: block;
+  .dollar-graphic__wrapper {
+    padding: 32px 16px;
   }
-}
-@media (max-width: 1068px) {
-  .dollar-graphic {
-    padding: 0 5px;
+  .dollar-graphic__content {
+    padding: 20px;
+  }
+  .dollar-graphic__description {
+    flex-direction: column;
+    gap: 0px;
+    margin-bottom: 8px;
+  }
+  .dollar-graphic__variation h1 {
+    font-size: 18px;
+    margin-bottom: 2px;
+  }
+  .dollar-graphic__variation p {
+    font-size: 14px;
+  }
+  .dollar-graphic__exchange-rate {
+    flex-direction: column;
+    gap: 0px;
+    margin-bottom: 8px;
+  }
+  .dollar-graphic__exchange-rate p {
+    font-size: 16px;
+    margin-bottom: 0px;
   }
 }
 </style>
